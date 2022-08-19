f8b6bb555131e5be3ddfa4ad5de669814afbc181
Abuh Solomon <53456402+Abuh05@users.noreply.github.com>
2022-08-19 15:34:19 +0200

    New update

---
+++ src/component/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='banner w-full md:w-2/3 px-7 mx-auto relative flex items-center justify-between'>
+        <div className='banner-description w-full md:w-1/2 p-3'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Banner
